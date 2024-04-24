3e25083938eeb7bf85dd6703dd9e32c495b0a8a7
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2024-04-24 00:09:38 +0000

    feat: added ex3

---
+++ ex3/ex3.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+theme_set(theme_dark())
+
+
+data <- readxl::read_excel("./electricity.xlsx")
+product <- "Renewables"
+min_year <- 2015
+countries <- c("Italy", "Latvia", "IEA Total")
+
+filtered_data <- subset(data, PRODUCT == product & YEAR >= min_year & COUNTRY %in% countries)
+
+filtered_data$TIME <- as.Date(paste(filtered_data$TIME, "01"), format = "%B %Y %d")
+
+filtered_data |>
+  ggplot(aes(x = TIME, y = share * 100, color = COUNTRY)) +
+  scale_y_continuous(limits = c(0, 100)) +
+  geom_line() +
+  labs(title = paste("Monthly evolution of Renewable energies market share of Italy vs Latvia vs IEA Total"),
+       x = paste("Date"),
+       y = paste("Market Share Percentage (%)"))
