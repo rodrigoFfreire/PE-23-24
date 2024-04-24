7e46a4ab62bd458b7ec828f4aeb27de6e3eec893
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2024-04-24 00:09:09 +0000

    fix: fixed ex2

--- ex2/ex2.R
+++ ex2/ex2.R
@@ -6,19 +6,11 @@ data <- read.csv(url, check.names = FALSE)
 target_year <- 2002
 age_group <- "55-74 years"
 
-filtered_data <- subset(data, year == target_year & age == age_group)
-
-male_data <- subset(filtered_data, sex == "male")
-
-female_data <- subset(filtered_data, sex == "female")
-
-male_plot <- ggplot(male_data, aes(x = `country`, y = `suicides/100k pop`)) +
-  geom_boxplot() +
-  labs(title = paste("Male suicides / 100k Inhabitants (", target_year, ", ", age_group, ")"))
-
-female_plot <- ggplot(female_data, aes(x = `country`, y = `suicides/100k pop`)) +
-  geom_boxplot() +
-  labs(title = paste("Female suicides / 100k Inhabitants (", target_year, ",", age_group, ")"))
-
-print(male_plot)
-print(female_plot)
+data |>
+  subset(year == target_year & age == age_group) |>
+  ggplot(aes(color = `sex`)) +
+  geom_boxplot(aes(x = `sex`, y = `suicides/100k pop`)) +
+  coord_flip() +
+  labs(title = paste("Suicides/100k Inhabitants - Males vs Females (", target_year, ",", age_group, ")"),
+       x = paste("Sex"),
+       y = paste("Suicides/100k Inhabitants"))
