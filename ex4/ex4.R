2fc662a1c53d148815266e38120b64ea5cb71b0f
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2024-04-24 16:16:19 +0100

    feat: added ex4

--- ex4/ex4.R
+++ ex4/ex4.R
@@ -4,20 +4,22 @@ n_sims <- 150
 beep_code <- 2
 off_code <- 1
 
+notoff <- 0
 beeps <- 0
-offs <- 0
-
 
 for (i in 1:n_sims) {
   signals <- sample(1:10, 9, replace = TRUE, prob = (1:10)/55)
   
-  if (beep_code %in% signals) {
-    beeps <- beeps + 1
-  }
-  
-  if (off_code %in% signals) {
-    offs <- offs + 1
+  if (!(off_code) %in% signals) {
+    notoff <- notoff + 1
+    
+    if (beep_code %in% signals) {
+      beeps <- beeps + 1
+    }
   }
 }
 
-print(round(beeps / (n_sims - offs), 2))
+notoff_prob <- notoff / n_sims
+beeps_prob <- beeps / n_sims
+
+print(round(beeps_prob/notoff_prob,2))
