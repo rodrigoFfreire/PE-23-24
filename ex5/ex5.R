d2f21776e17067865a2fdf5b6f7f9ab637a499ba
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2024-04-24 21:24:38 +0000

    possible fix to ex5

--- ex5/ex5.R
+++ ex5/ex5.R
@@ -4,28 +4,21 @@ n <- 4
 r <- 150
 m <- 130
 
-calc_T <- function(z) {
-  sqrt(n) * (z[1] / sqrt(sum(z[-1]^2)))
+samples <- matrix(NA, nrow = r, ncol = m)
+
+for (i in 1:r) {
+  for (j in 1:m) {
+    z <- rnorm(n + 1)
+    samples[i, j] <- (sqrt(n) * z[1] / sqrt(sum(z[-1]^2)))
+  }
 }
 
-# Gerar as amostras
-samples <- matrix(rnorm(r * (m + 1)), ncol = m + 1)
+proportions <- apply(samples, 1, function(row) mean(row <= -0.9))
 
-# Calcular T para cada amostra
-Ts <- apply(samples, 1, calc_T)
+p <- mean(proportions)
 
-# Calcular a proporção de valores menores ou iguais a -0.9 em cada amostra
-proportions <- mean(Ts <= -0.9)
+direct_p <- pt(-0.9, df = n)
 
-# Calcular a média das proporções
-mean_proportion <- mean(proportions)
+diff_abs <- abs(p - direct_p) * 100
 
-# Calcular a probabilidade direta usando a função pt do R
-direct_prob <- pt(-0.9, df = n)
-
-# Calcular a diferença absoluta
-diff_abs <- abs(mean_proportion - direct_prob) * 100
-
-# Arredondar o resultado final
-result <- round(diff_abs, 5)
-result
+diff_abs
