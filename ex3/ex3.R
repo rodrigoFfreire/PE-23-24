94e8bb962f9c250b71c6686d647f4cdb711637ab
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2024-04-24 19:49:28 +0100

    fixed ex3

--- ex3/ex3.R
+++ ex3/ex3.R
@@ -11,10 +11,14 @@ filtered_data <- subset(data, PRODUCT == product & YEAR >= min_year & COUNTRY %i
 
 filtered_data$TIME <- as.Date(paste(filtered_data$TIME, "01"), format = "%B %Y %d")
 
+filtered_data$share <- as.numeric(filtered_data$share)
+
 filtered_data |>
   ggplot(aes(x = TIME, y = share * 100, color = COUNTRY)) +
   scale_y_continuous(limits = c(0, 100)) +
+  scale_x_date(date_breaks = "5 months", date_labels = "%B %Y") +
   geom_line() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   labs(title = paste("Monthly evolution of Renewable energies market share of Italy vs Latvia vs IEA Total"),
        x = paste("Date"),
        y = paste("Market Share Percentage (%)"))
